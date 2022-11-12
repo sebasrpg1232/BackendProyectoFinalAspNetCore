1837c2d32058fd8fb93eec0712bd91bc2f844d1d
LAPTOP <sebasrpg1232@gmail.com>
2022-11-11 22:19:34 -0500

    mejora?

---
+++ BackendProyectoFinal/Migrations/20221111153434_Initial Migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendProyectoFinal.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    driverId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    driverName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    driverDirection = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    driverEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    driverNumber = table.Column<long>(type: "bigint", nullable: false),
+                    driverPassword = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.driverId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drivers");
+        }
+    }
+}
